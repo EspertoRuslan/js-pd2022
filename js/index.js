5c8e46fb70560a77db472ccda3f6f83b0a3dc179
EspertoRuslan <itpochta25@gmail.com>
2022-05-08 16:12:55 +0300

    первое занятие JS

---
+++ js/index.js
@@ -0,0 +1,218 @@
+//alert("Hello World");
+console.log("Hello Vasya");
+console.log("Hello, ", "Java Sc", "!");
+/*
+let x;
+let y;
+let UserName;
+let a, s, d, b;
+let userColorAvatarBG;
+
+x=5;
+y=100;
+b=-400;
+d=3.14;
+s=0.007;
+UserName="Vasya";
+console.log(x);
+console.log("User Name is", UserName);
+
+let t = 300;
+let name1="Tom Fox";
+console.log(name1);
+
+let x2;
+x2=100;
+console.log(x2);
+x2=-3;
+console.log(x2);
+x2="Hello";
+console.log(x2);
+x2="Vasya";
+console.log(x2);
+x2=true;
+console.log(x2);
+
+console.log("x=",x);
+console.log("y=", y);
+x=y;
+console.log("x=",x);
+console.log("y=", y);
+
+y=2;
+x=10;
+
+console.log("x=",x);
+x=x+y;
+console.log("x=",x);
+x=x*y;
+console.log("x=",x);
+x=x-y;
+console.log("x=",x);
+
+let a1=10;
+let a2=0;
+let a3="Ruslan";
+let a4=falce;
+let a5=-2;
+let result;
+result=a1+a5;
+result=a1-a5;
+result=a1*a5;
+result=a1/a5;
+console.log(result);
+
+result=result*2;
+console.log(result);
+
+result=500-result;
+console.log(result);
+
+*/
+
+const COLOR_RED="#FF0000";
+const COLOR_GREEN="#00FF00";
+const COLOR_BLUE="#000FF";
+const MAX_QUERIES=3;
+/*
+let x=100;
+x=x+MAX_QUERIES;
+console.log(x);
+/*
+let a1=200, let a2=100, result;
+a1+=5; //a1=a1+5;
+a1-=5;
+a1*=5;
+a1/=5;
+
+a2 +=a1; //a2=a2+a1;
+*/
+/*
+x=10;
+x=x+1;
+x++ //увеличивет на 1 или ++x
+x--//уменьшает на 1
+
+result = 5**2;
+console.log(result);
+
+result = 5**(1/2);
+console.log(result);
+
+let a=10;
+let b=20;
+let c=a-b;
+console.log(c);
+let d=c;
+result=c+d;
+console.log(result);
+
+let num = 100;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+num =+ 1;
+num -+ 1;
+
+let r=prompt('Введите число');
+console.log(r);
+result = r**2;
+console.log(result);
+*/
+/*
+let a=prompt('Введите ширину');
+let b=prompt('Введите длину');
+let S=a*b;
+result = S;
+console.log(result);
+
+let c=prompt('миллиметры');
+let d=c/10;
+console.log(d);
+*/
+let a, b;
+a=5;
+b=7;
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+/*
+let userAge=prompt('Введите свой возрас');
+if(userAge >= 18) {
+    alert('Вы - совершеннолетний');
+}
+*/
+/*
+let x;
+if (x >=0){
+    console.log('положительное число');
+
+}
+
+if(условие){
+    
+}
+*/
+
+/*
+let userAge=prompt('Введите свой возрас');
+if(userAge >= 18) {
+    alert('Вы - совершеннолетний');
+}else{
+    alert('Вы - несовершеннолетний');
+}
+*/
+
+/*
+let Number=prompt('Введите число');
+if(Number >= 10) {
+    alert('больше десяти');
+}else{
+    alert('меньше десяти');
+}
+*/
+/*
+let x=prompt('Enter x');
+if(x>0){
+    console.log('положительное');
+}else if(x<0){
+    console.log('отрицательное');
+}else if(x=0){
+    console.log('равно нулю');
+}
+*/
+
+/*
+let userAge=18;
+let res=(userAge >=18) ? 'совершеннолетний' : 'несовершенолетний';
+console.log(res);
+*/
+/*
+let Number=prompt('Введите число');
+let res=(Number %2==0) ? 'четное' : 'нечётное';
+console.log(res);
+
+if(Number>0 && Number>10){}
+
+if(Number>0 || Number>10){}
+
+if(weekDay == "Sunday" && isWorking == true){console.log('si');}
+if(surface == "бумага")
+*/
+
+//cikle
+
+let start=1, end=10, result=0;
+for(start=1; start<=end; start=start+1){
+    result=result+start;
+}
+console.log(result);
+
+let i;
+for (i=1; i<=10; i++);{
+    console.log(i);
+}
+
